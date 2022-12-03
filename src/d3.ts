ab5220271cd3ea469ce0af30c5511637ed846b29
Sasu Mäkinen <sasu.makinen@mindler.se>
2022-12-03 14:27:35 +0200

    d3

---
+++ src/d3.ts
@@ -0,0 +1,53 @@
+import { map, match } from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/function";
+import { getFileContents, splitLines, sum } from "./util";
+
+const splitInMiddle = (arr: string[]) =>
+  arr.map((str) => [
+    str.slice(0, str.length / 2),
+    str.slice(str.length / 2, str.length),
+  ]);
+
+const mapPriority = (arr: string[]): number[] =>
+  arr.map((str) =>
+    str.charCodeAt(0) < 91 ? str.charCodeAt(0) - 38 : str.charCodeAt(0) - 96
+  );
+const reduceUniquesOfN = (arrs: string[][]) =>
+  arrs.map((arrN: string[]) => {
+    const join = [...arrN.join()].filter((char) =>
+      arrN.every((arr) => [...arr].includes(char))
+    );
+    return join;
+  });
+
+const flatMapUnique = (arrs: string[][]) => {
+  return arrs.flatMap((arr: string[]) => [...new Set(arr)]);
+};
+pipe(
+  "src/inpufz/i3.txt",
+  getFileContents,
+  map(splitLines),
+  map(splitInMiddle),
+  map(reduceUniquesOfN),
+  map(flatMapUnique),
+  map(mapPriority),
+  map(sum),
+  match(console.error, console.log)
+);
+
+const chunk = (arr: string[], size: number): string[][] =>
+  arr.length > size
+    ? [arr.slice(0, size), ...chunk(arr.slice(size), size)]
+    : [arr];
+
+pipe(
+  "src/inpufz/i3.txt",
+  getFileContents,
+  map(splitLines),
+  map((z) => [...chunk(z, 3)]),
+  map(reduceUniquesOfN),
+  map(flatMapUnique),
+  map(mapPriority),
+  map(sum),
+  match(console.error, console.log)
+);
--- src/util.ts
+++ src/util.ts
@@ -6,3 +6,10 @@ export const getFileContents = (path: string) =>
   tryCatch(() => readFileSync(path, "utf-8"), toError);
 
 export const sum = (arr: number[]) => arr.reduce((a, b) => a + b);
+
+export const splitLines = (str: string) => str.split("\n");
+
+export const peek = <T>(z: T): T => {
+  console.log(z);
+  return z;
+};
